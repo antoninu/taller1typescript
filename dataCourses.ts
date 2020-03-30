3cad181417181c6cb539c3a3a5aee6f31a616038
Antonio Aspite <antonioaspite@gmail.com>
2020-03-30 01:25:45 -0500

    first commit

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en Equipo", "Rubby Casallas", 5),
+  new Course("Lenguajes y Máquinas", "Silvia Takahashi", 2),
+  new Course("Sistemas Transaccionales", "Germán", 2),
+  new Course("DALGO", "Nelson", 1),
+  new Course("Infraestructura Computacional", "Ana Maria", 6)
+]
---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+import { dataCourses } from "./dataCourses.js";
+var coursesTbody = document.getElementById('courses'); // Nodo tbody que tiene el id="courses"
+var btnfilterByName = document.getElementById("button-filterByName");
+var inputSearchBox = document.getElementById("search-box");
+var totalCreditElm = document.getElementById("total-credits");
+btnfilterByName.onclick = function () { return applyFilterByName(); };
+totalCreditElm.innerHTML = "" + getTotalCredits(dataCourses);
+console.log(coursesTbody);
+function renderCoursesInTable(courses) {
+    courses.forEach(function (c) {
+        console.log(c);
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td scope=\"row\">" + c.name + "</td>\n                           <td>" + c.professor + "</td>\n                           <td>" + c.credits + "</td>";
+        coursesTbody.appendChild(trElement);
+    });
+}
+function getTotalCredits(courses) {
+    var totalCredits = 0;
+    courses.forEach(function (course) { return totalCredits = totalCredits + course.credits; });
+    return totalCredits;
+}
+function applyFilterByName() {
+    var text = inputSearchBox.value;
+    text = (text == null) ? '' : text;
+    clearCoursesInTable();
+    var coursesFiltered = searchCourseByName(text, dataCourses);
+    renderCoursesInTable(coursesFiltered);
+}
+function searchCourseByName(nameKey, courses) {
+    return nameKey === '' ? dataCourses : courses.filter(function (c) {
+        return c.name.match(nameKey);
+    });
+}
+function clearCoursesInTable() {
+    while (coursesTbody.hasChildNodes()) {
+        if (coursesTbody.firstChild != null) {
+            coursesTbody.removeChild(coursesTbody.firstChild);
+        }
+    }
+}
+renderCoursesInTable(dataCourses);
+getTotalCredits(dataCourses);
